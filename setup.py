8ebe003a7c4d78a9131858eb3ae9b646b9954755
David Andreoletti <david.andreoletti.dev@gmail.com>
2012-03-13 10:57:29 +0800

    Changed python project name: pyconvert.

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 
 setup(
-    name='media.convert.application',
+    name='pyconvert',
     version='0.0.1dev',
     author='David Andreoletti',
     packages=find_packages(),
-    description='Convert files for storage into my own aperture libraries.',
+    description='Python application to convert files for storage into my own aperture libraries.',
 )
 
 print "packages==>" + str(find_packages())
