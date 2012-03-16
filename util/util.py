1f118c8d4c733e3e6622b07ceb8fde49fc52eed0
David Andreoletti <david.andreoletti.dev@gmail.com>
2012-03-16 15:39:38 +0800

    Added generic methods to deal with character escaping for various shell OSes

--- util/util.py
+++ util/util.py
@@ -4,3 +4,41 @@ def enum(*sequential, **named):
     """
     enums = dict(zip(sequential, range(len(sequential))), **named)
     return type('Enum', (), enums)
+
+def escapeAllWith(aString, anEscapeString):
+    """
+    Escape all characters with the escape string.
+    @param aString String to escape
+    @param anEscapeString String to escape the first string with
+    @return Escaped string
+    """
+    escapedString = list()
+    for c in aString:
+        escapedString.append(anEscapeString)
+        escapedString.append(c)
+
+    return ''.join(escapedString)
+
+def escapePathForUnix(aString):
+    """
+    Escape all characters with the escape string.
+    @param aString String to escape
+    @param anEscapeString String to escape the first string with
+    @return Escaped string
+    """
+    path = escapeAllWith(aString,"\\")
+    return path
+
+def escapePathForOSIndependentShell(aString):
+    """
+    Escape all characters with the escape string.
+    @param aString String to escape
+    @param anEscapeString String to escape the first string with
+    @return Escaped string
+    """
+    return escapePathForUnix(aString)
+    
+    
+    
+    
+
