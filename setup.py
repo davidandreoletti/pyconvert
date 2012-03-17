f04892b368dff90c41529ff6b80912c085b0833a
davidandreoletti <david.andreoletti.dev@gmail.com>
2012-03-17 23:55:04 +0800

    Added minimum Python version check at installation time

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
+
+pythonPackageName="pyconvert"
+pythonPackageVersion="0.0.1"
+pythonPackageFullName=pythonPackageName + " " + pythonPackageVersion
+
+# Check minimun Python interpreter version required
+import sys
+if not hasattr(sys, "hexversion") or sys.hexversion < 0x020701f0:
+    sys.stderr.write("Installation FAILED: " + pythonPackageFullName +
+                     " requires Python 2.7.1 or better.\n")
+    sys.exit(1)
+
 import ez_setup
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
 setup(
-    name='pyconvert',
-    version='0.0.1dev',
+    name=pythonPackageName,
+    version=pythonPackageVersion,
     author='David Andreoletti',
     packages=find_packages(),
     description='Python application to convert files for storage into my own aperture libraries.',
