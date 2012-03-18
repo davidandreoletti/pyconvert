62a9ca8a5274f7b493fe8bcb356d81ca3ff06b80
davidandreoletti <david.andreoletti.dev@gmail.com>
2012-03-18 12:12:37 +0800

    Added package dependencies

--- setup.py
+++ setup.py
@@ -21,7 +21,18 @@ setup(
     version=pythonPackageVersion,
     author='David Andreoletti',
     packages=find_packages(),
-    description='Python application to convert files for storage into my own aperture libraries.',
+    description="Python application to convert files for storage into " +
+                 "my own aperture libraries.",
+    install_requires=['pycl==0.0.1',
+                      'pymedia==0.0.1',
+                      'pyhandbrake==0.0.1',
+                      'pyffmpeg==0.0.1',
+                      'pyimagemagick==0.0.1'],
+    dependency_links = ['https://github.com/davidandreoletti/pycl/zipball/v0.0.1#egg=pycl-0.0.1',
+                        'https://github.com/davidandreoletti/pymedia/zipball/v0.0.1#egg=pymedia-0.0.1',
+                        'https://github.com/davidandreoletti/pyhandbrake/zipball/v0.0.1#egg=pyhandbrake-0.0.1',
+                        'https://github.com/davidandreoletti/pyffmpeg/zipball/v0.0.1#egg=pyffmpeg-0.0.1',
+                        'https://github.com/davidandreoletti/pyimagemagick/zipball/v0.0.1#egg=pyimagemagick-0.0.1'],
 )
 
 print "packages==>" + str(find_packages())
